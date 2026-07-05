1e93841101f7a50ff7ca5c45b3249c02bb3a938c
Harshit Pathak <194086212+AARAYHARSHIT@users.noreply.github.com>
2026-07-05 18:38:52 +0530

    Update script.js

--- backend/script.js
+++ backend/script.js
@@ -1,10 +1,26 @@
-const express =require("express");
+// const express =require("express");//here in const express express is variablle we can name it anything else to save Express 
+// const app = express();// when we ran express using the given command express's all power got into app named variable
+// //simply app also have same capabilities as express
+// app.get("/",(req,res)=>{  //using the app function we create a get route ie. '/'
+//     res.send("Express is working!");
+// });
+
+// app.listen(3000, ()=>{
+//     console.log("server running on port 3000");
+// });
+
+const express =require("exxpress");
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Express is working!");
+app.use(function(req,res,next){
+    next();
 });
 
-app.listen(3000, ()=>{
-    console.log("server running on port 3000");
+app.get("/",function(req,res,next){
+    res.send("hello world");
+});
+
+app.get("/profile",function(req,res){
+    res.send("hello from profile");
+
 });
